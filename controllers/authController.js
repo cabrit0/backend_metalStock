6ad0d4f51c43dece2826da6b97f68cb584a92cbd
cabrit0 <cabrit0.dev@gmail.com>
2026-01-18 19:33:13 +0000

    test big v1

--- controllers/authController.js
+++ controllers/authController.js
@@ -139,7 +139,7 @@ const login = async (req, res) => {
         res.status(500).json({
             success: false,
             message: 'Erro ao fazer login',
-            error: error.message // Temporarily show error for debugging
+            error: process.env.NODE_ENV === 'development' ? error.message : undefined
         });
     }
 };
